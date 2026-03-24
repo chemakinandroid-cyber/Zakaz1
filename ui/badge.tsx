acefadcada7443ca68edfbe9521e9d4bd7cf1c6e
chemakinandroid-cyber <chemakinandroid@gmail.com>
2026-03-24 23:28:32 +0800

    Delete ui directory

--- ui/badge.tsx
+++
@@ -1,8 +0,0 @@
-
-export function Badge({ children, className="" }: any) {
-  return (
-    <span className={"px-2 py-1 text-xs rounded-full bg-neutral-200 " + className}>
-      {children}
-    </span>
-  );
-}
